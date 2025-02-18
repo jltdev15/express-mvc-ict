69839594f9aa1737046cbd3927f20d5673b8de24
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2025-02-18 14:54:20 +0800

    initialize express application with user model and routes

---
+++ Model/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// 1 Create an schema
+const usersSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+
+// 2 Create model for your schema
+const User = mongoose.model("User", usersSchema);
+
+exports.module = User; // Export the model
